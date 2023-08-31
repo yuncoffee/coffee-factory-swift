e1d0c90824651b1c69d5a3e025cde511fbec1ff2
yuncoffee <dong072815@gmail.com>
2023-09-01 01:42:33 +0900

    [Test] extension-7

--- Sources/coffee-factory-swift/Foundation/CGFloat+Extension.swift
+++ Sources/coffee-factory-swift/Foundation/CGFloat+Extension.swift
@@ -8,8 +8,8 @@
 import Foundation
 
 public protocol SpacingSize {
-    static var spacing100: CGFloat { get set }
-    static var spacing800: CGFloat { get set }
+    static var spacing100: CGFloat { get }
+    static var spacing800: CGFloat { get }
 }
 //
 //extension SpacingSize {
@@ -18,45 +18,8 @@ public protocol SpacingSize {
 //    }
 //}
 
-extension CGFloat: SpacingSize {
-    private static var _spacing100 = 20.0
-    private static var _spacing800 = 80.0
-    
-    public static var spacing100: CGFloat {
-        get {
-            self._spacing100
-        }
-        set {
-            self._spacing100 = newValue
-        }
+public extension SpacingSize {
+    static var spacing100: CGFloat{
+            20.0
     }
-
-    public static var spacing800: CGFloat {
-        get {
-            self._spacing800
-        }
-        set {
-            self._spacing800 = newValue
-        }
-    }
-//
-//    public static var spacing100: CGFloat {
-//        20.0
-//    }
-//
-//    public static var spacing400: CGFloat {
-//        40.0
-//    }
-//
-//    public static var spacing500: CGFloat {
-//        60.0
-//    }
-//
-//    public static var spacing700: CGFloat {
-//        80.0
-//    }
-//
-//    public static var spacing800: CGFloat {
-//        120.0
-//    }
 }
