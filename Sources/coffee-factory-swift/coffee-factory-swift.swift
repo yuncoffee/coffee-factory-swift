df613f0f6aa0795282cc1ec181331695c1de69d3
yuncoffee <dong072815@gmail.com>
2023-09-01 01:47:11 +0900

    [Chore] fix

--- Sources/coffee-factory-swift/coffee-factory-swift.swift
+++ Sources/coffee-factory-swift/coffee-factory-swift.swift
@@ -3,7 +3,7 @@ import Foundation
 public struct CoffeeFactorySwift {
     public private(set) var text = "Hello, World!"
 
-    public var myFloat: CGFloat = .spacing100
+//    public var myFloat: CGFloat = .spacing100
     
     public init() {
     }
