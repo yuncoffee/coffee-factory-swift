ada1e412fe85bb1f493d6572e8ed57553a097b38
yuncoffee <dong072815@gmail.com>
2023-09-02 13:24:23 +0900

    [Test] Change module name

--- Package.swift
+++ Package.swift
@@ -9,8 +9,8 @@ let package = Package(
     products: [
         // Products define the executables and libraries a package produces, and make them visible to other packages.
         .library(
-            name: "CoffeeFactorySwift",
-            targets: ["coffee-factory-swift"]),
+            name: "coffee-factory-swift",
+            targets: ["CoffeeFactorySwift"]),
     ],
     dependencies: [
         // Dependencies declare other packages that this package depends on.
@@ -20,11 +20,11 @@ let package = Package(
         // Targets are the basic building blocks of a package. A target can define a module or a test suite.
         // Targets can depend on other targets in this package, and on products in packages this package depends on.
         .target(
-            name: "coffee-factory-swift",
+            name: "CoffeeFactorySwift",
             dependencies: []),
         .testTarget(
             name: "coffee-factory-swift-tests",
-            dependencies: ["coffee-factory-swift"]),
+            dependencies: ["CoffeeFactorySwift"]),
     ],
     swiftLanguageVersions: [.v5]
 )
