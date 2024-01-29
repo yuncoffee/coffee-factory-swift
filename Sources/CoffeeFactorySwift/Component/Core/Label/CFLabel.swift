fba8ff72108802f16df5dc3ca8605dd42fa7ed28
yuncoffee <dong072815@gmail.com>
2024-01-29 23:37:43 +0900

    [add]: add cflabel component

---
+++ Sources/CoffeeFactorySwift/Component/Core/Label/CFLabel.swift
@@ -0,0 +1,18 @@
+//
+//  SwiftUIView.swift
+//  
+//
+//  Created by yuncoffee on 1/29/24.
+//
+
+import SwiftUI
+
+struct SwiftUIView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    SwiftUIView()
+}
